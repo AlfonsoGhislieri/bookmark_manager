1a0c85e3f9fc6ff4d07cdd6a5f0268fcbc010c47
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-07 17:12:35 +0000

    Implement title in feature test

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -2,6 +2,7 @@ feature 'adding bookmarks' do
   scenario 'user fills in form and submits' do
     visit('/bookmarks')
     fill_in :bookmark_url, with: 'www.sherwin.com'
+    fill_in :bookmark_title, with: 'Sherwin'
     click_button 'Submit'
     expect(page).to have_content 'www.sherwin.com'
   end
